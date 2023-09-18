f7d4af0e049100a01f2d0acec3ddfe2103fefc9d
lliurex <juanma1980@gmail.com>
2023-09-18 11:41:16 +0200

    remove force-all

--- llxupgrader.py
+++ llxupgrader.py
@@ -122,7 +122,7 @@ def _generateDemoteScript():
 			fcontent+="case \"$ACTION\" in\n" 
 			fcontent+="preActions)\n"
 			fcontent+="dpkg --force-all --purge {}\n".format(" ".join(demote))
-			fcontent+="apt-get upgrade --force-yes\n"
+			fcontent+="apt-get upgrade -y --allow-remove-essential --allow-change-held-packages --only-upgrade\n"
 			fcontent+="rm $0\n"
 			fcontent+="\n;;\nesac"
 		with open(LLXUPSCRIPT,"w") as f:

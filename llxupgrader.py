d1317bd0518996dc12fe24a7f162c52643666a32
lliurex <juanma1980@gmail.com>
2023-09-19 06:43:30 +0200

    force apt fix after removing demoted

--- llxupgrader.py
+++ llxupgrader.py
@@ -122,7 +122,7 @@ def _generateDemoteScript():
 			fcontent+="case \"$ACTION\" in\n" 
 			fcontent+="preActions)\n"
 			fcontent+="dpkg --force-all --purge {} || true\n".format(" ".join(demote))
-			fcontent+="apt-get install -f\n"
+			fcontent+="apt-get install -f -y\n"
 			fcontent+="rm $0\n"
 			fcontent+="\n;;\nesac"
 		with open(LLXUPSCRIPT,"w") as f:

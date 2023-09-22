9bc2cd88913b902dfce3cf2b4facab1499497716
lliurex <juanma1980@gmail.com>
2023-09-22 09:46:14 +0200

    Add reboot

--- upgrader.py
+++ upgrader.py
@@ -1,7 +1,16 @@
 #!/bin/bash
-xinit lliurex-up $* -- :0 vt$XDG_VTNR 
+sleep 1
+service plymouth stop
+service plymouth-start stop
+llxup='/sbin/lliurex-up -u -s -n'
+xinit $llxup $* -- :0 vt$XDG_VTNR &
+sleep 5
+service plymouth stop
+service plymouth-start stop
+chvt $XDG_VTNR
 while [ ! -e /tmp/.updateEnd ] 
 do
+	echo "WAIT"
 	sleep 10
 done
-
+kdialog --title "Lliurex Release Upgrade" --msgbox "Press to reboot" && reboot

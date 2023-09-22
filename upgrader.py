7866dd9e4258804d1b2a067a77560d122f101412
lliurex <juanma1980@gmail.com>
2023-09-22 11:13:23 +0200

    ensure we show something

--- upgrader.py
+++ upgrader.py
@@ -3,14 +3,17 @@ sleep 1
 service plymouth stop
 service plymouth-start stop
 llxup='/sbin/lliurex-up -u -s -n'
-xinit $llxup $* -- :0 vt$XDG_VTNR &
-sleep 5
+xinit $llxup $* -- :0 vt1 &
+sleep 2
+DISPLAY=:0 QT_QPA_PLATFORM=linuxfb kdialog --title "Lliurex Release Upgrade" --msgbox "Setting up things. Wait for Lliurex Up"
+sleep 7
 service plymouth stop
 service plymouth-start stop
-#chvt $XDG_VTNR
+chvt 1
 while [ ! -e /tmp/.updateEnd ] 
 do
 	echo "WAIT"
 	sleep 10
 done
-kdialog --title "Lliurex Release Upgrade" --msgbox "Press to reboot" && reboot
+kdialog --title "Lliurex Release Upgrade" --msgbox "Press to reboot"  
+systemctl reboot

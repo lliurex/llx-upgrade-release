8add9ab01fc023da2542fcfeccf86d3e379f5eba
lliurex <juanma1980@gmail.com>
2023-09-23 10:40:08 +0200

    improvements

--- src/upgrader.py
+++ src/upgrader.py
@@ -1,19 +1,17 @@
 #!/bin/bash
-sleep 1
 service plymouth stop
 service plymouth-start stop
-llxup='/sbin/lliurex-up -u -s -n'
-xinit $llxup $* -- :0 vt1 &
+LLXUP='/sbin/lliurex-up -u -s -n'
+KWIN=$(which kwin)
+xinit $KWIN $* -- :0 vt1 &
+export DISPLAY=:0
 sleep 2
-DISPLAY=:0 QT_QPA_PLATFORM=linuxfb kdialog --title "Lliurex Release Upgrade" --msgbox "Setting up things. Wait for Lliurex Up"
-sleep 7
-service plymouth stop
-service plymouth-start stop
-chvt 1
-while [ ! -e /tmp/.updateEnd ] 
-do
-	echo "WAIT"
-	sleep 10
-done
-kdialog --title "Lliurex Release Upgrade" --msgbox "Press to reboot"  
-systemctl reboot
+hostname lliurex.net
+/usr/share/llx-upgrade-release/fakenet.py &
+hostname lliurex.net
+$LLXUP 
+apt clean
+rm /etc/apt/apt.conf 2>/dev/null
+> /etc/apt/sources.list
+repoman-cli -e 0 -y
+kdialog --title "Lliurex Release Upgrade" --msgbox "Upgrade ended. Press to reboot"  && systemctl reboot || systemctl reboot

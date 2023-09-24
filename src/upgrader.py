11bf8c8e11e20a0d9fea2344fd5cb185018ba0c8
lliurex <juanma1980@gmail.com>
2023-09-25 00:13:03 +0200

    minor changes

--- src/upgrader.py
+++ src/upgrader.py
@@ -3,15 +3,14 @@ service plymouth stop
 service plymouth-start stop
 LLXUP='/sbin/lliurex-up -u -s -n'
 KWIN=$(which kwin)
-xinit $KWIN $* -- :0 vt1 &
-export DISPLAY=:0
-sleep 2
-hostname lliurex.net
-/usr/share/llx-upgrade-release/fakenet.py &
-hostname lliurex.net
+xinit $KWIN -- :0 vt5 &
+sleep 1
+#hostname lliurex.net
+#/usr/share/llx-upgrade-release/fakenet.py &
+#hostname lliurex.net
 $LLXUP 
 rm /etc/apt/apt.conf 2>/dev/null
 echo -n "" > /etc/apt/sources.list
 apt clean
 repoman-cli -e 0 -y
-kdialog --title "Lliurex Release Upgrade" --msgbox "Upgrade ended. Press to reboot"  && systemctl reboot || systemctl reboot
+kdialog --title "Lliurex Release Upgrade" --msgbox "Upgrade ended. Press to reboot"  && systemctl reboot 

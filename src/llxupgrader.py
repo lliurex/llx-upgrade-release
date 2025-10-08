013ac90527d3badcd1d4ae1d1b5bbf8121b58dd9
juanma <juanma1980@gmail.com>
2025-10-09 01:59:15 +0200

    Fix typo

--- src/llxupgrader.py
+++ src/llxupgrader.py
@@ -535,7 +535,7 @@ def setSystemdUpgradeTarget():
 	with open (target,"w") as f:
 		f.write("\n".join(targetContent))
 	unitContent=["[Unit]","Description=Upgrade environment","Documentation=man:sulogin(8)","DefaultDependencies=no","Conflicts=network-manager.service","Conflicts=shutdown.target","Conflicts=llx-upgrade.service","Before=shutdown.target","Before=llx-upgrade.service"]
-	serviceContent=["[Service]","Environment=HOME=/root","WorkingDirectory=-/root","ExecStart=-/usr/share/llx-upgrade-release/upgrader.py","Type=idle","#StandardInput=tty-force","StandardOutput=inherit","StandardError=inherit","KillMode=process","IgnoreSIGPIPE=no","SendSIGHUP=yes"]
+	serviceContent=["[Service]","Environment=HOME=/root","WorkingDirectory=-/root","ExecStart=/usr/share/llx-upgrade-release/upgrader.py","Type=idle","#StandardInput=tty-force","StandardOutput=inherit","StandardError=inherit","KillMode=process","IgnoreSIGPIPE=no","SendSIGHUP=yes"]
 	with open (service,"w") as f:
 		f.write("\n".join(unitContent))
 		f.write("\n")
